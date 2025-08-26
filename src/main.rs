b31034fd8558e4664fb9653c8595857a60eacb4e
朱重迁 <z32664206862022@163.com>
2025-08-26 16:47:53 +0800

    project init add actix-web

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("0.0.0.0", 2345))?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
